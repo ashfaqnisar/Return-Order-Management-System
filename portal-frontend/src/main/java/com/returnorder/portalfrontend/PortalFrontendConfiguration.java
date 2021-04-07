ed1b66300dbd4cf2a5b1e9f68b2e45abe90819b8
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2021-04-08 00:18:03 +0530

    fix: Removed the unrequired bean

--- portal-frontend/src/main/java/com/returnorder/portalfrontend/PortalFrontendConfiguration.java
+++ portal-frontend/src/main/java/com/returnorder/portalfrontend/PortalFrontendConfiguration.java
@@ -6,8 +6,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class PortalFrontendConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/files/**").addResourceLocations("/WEB-INF/pdf/");
-    }
 }
