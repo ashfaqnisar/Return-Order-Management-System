ba7122e823f68d6f13e96bed390d61a73eab0199
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2021-04-10 04:42:51 +0530

    feat: Added the authentication to the component processing service.

--- payment-service/src/main/java/com/returnorder/payment/controller/CreditCardController.java
+++ payment-service/src/main/java/com/returnorder/payment/controller/CreditCardController.java
@@ -2,7 +2,6 @@ package com.returnorder.payment.controller;
 
 import com.returnorder.payment.payload.PaymentResponse;
 import com.returnorder.payment.service.CreditCardService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
