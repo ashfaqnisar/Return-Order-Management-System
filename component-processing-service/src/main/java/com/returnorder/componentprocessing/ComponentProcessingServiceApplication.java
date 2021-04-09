60ec830f90549455cacfa377f94694586c1c7052
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2021-04-10 02:05:57 +0530

    feat: Added the sample component processing

---
+++ component-processing-service/src/main/java/com/returnorder/componentprocessing/ComponentProcessingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.returnorder.componentprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentProcessingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComponentProcessingServiceApplication.class, args);
+    }
+}
