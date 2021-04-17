f655b0cbecfc555e787288a3613ca407d5200e8b
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2021-04-17 21:12:54 +0530

    feat: Added the login page to the website

---
+++ portal-frontend/src/main/java/com/roms/portalfrontend/PortalConfiguration.java
@@ -0,0 +1,15 @@
+package com.roms.portalfrontend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class PortalConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+}
--- portal-frontend/src/main/java/com/roms/portalfrontend/controllers/PortalController.java
+++ portal-frontend/src/main/java/com/roms/portalfrontend/controllers/PortalController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
