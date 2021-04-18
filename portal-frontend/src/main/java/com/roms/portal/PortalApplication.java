a58ef770a801b9fee3f29f565288dec5c2c9ce22
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2021-04-18 23:45:31 +0530

    refractor: Changed the package name of portal

---
+++ portal-frontend/src/main/java/com/roms/portal/PortalApplication.java
@@ -0,0 +1,16 @@
+package com.roms.portalfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class PortalApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(PortalApplication.class, args);
+    }
+}
