029620331741087b414a8b4d7a04dda0d55f6df6
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2021-04-19 13:58:02 +0530

    feat: Added the config server to the application

--- portal-frontend/src/main/java/com/roms/portal/PortalApplication.java
+++ portal-frontend/src/main/java/com/roms/portal/PortalApplication.java
@@ -3,11 +3,11 @@ package com.roms.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 public class PortalApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
