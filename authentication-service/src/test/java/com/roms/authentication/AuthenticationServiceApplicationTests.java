405fd4e402b53b1793b640b98a32ac7d8466f5a5
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2021-04-19 22:03:45 +0530

    feat: Changed the background of the color

--- authentication-service/src/test/java/com/roms/authentication/AuthenticationServiceApplicationTests.java
+++ authentication-service/src/test/java/com/roms/authentication/AuthenticationServiceApplicationTests.java
@@ -1,13 +1,11 @@
 package com.roms.authentication;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthenticationServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
